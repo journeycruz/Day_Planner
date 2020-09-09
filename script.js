522d8fe7bf795b2789ec0668907d9766984bc463
Journey Cruz <journey.cruz@gmail.com>
2020-09-08 20:04:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ var currentDay = moment().format('dddd, MMMM Do');
 $("#currentDay").text(currentDay);
 var currentHour = moment().format('h A');
 // change to test class conditions for hour block color
-// var currentHour = "12 PM";
+var currentHour = "12 PM";
 console.log("currentHour: " + currentHour);
  
 // create array of all times
 var allTimes = ["12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 PM"];
 // get index of current hour
-var currentTimeIndex = "12 PM";
+var currentTimeIndex = allTimes.indexOf(currentHour);
 console.log("currentIndex: " + currentTimeIndex);
 // create array of work times
 var workTimes = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
