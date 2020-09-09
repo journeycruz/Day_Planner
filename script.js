2558232cc4d4ddc95d8823b6392ac3d4cb1f4437
Journey Cruz <journey.cruz@gmail.com>
2020-09-08 20:05:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // format current date and time
 var currentDay = moment().format('dddd, MMMM Do');
 $("#currentDay").text(currentDay);
-// var currentHour = moment().format('h A');
+var currentHour = moment().format('h A');
 // change to test class conditions for hour block color
-var currentHour = "12 PM";
+// var currentHour = "12 PM";
 console.log("currentHour: " + currentHour);
  
 // create array of all times
