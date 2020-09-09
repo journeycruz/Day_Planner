2b881536eb6a145039c085e1eeecc144507ec0ab
Journey Cruz <journey.cruz@gmail.com>
2020-09-08 20:01:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ console.log("currentHour: " + currentHour);
 // create array of all times
 var allTimes = ["12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 PM"];
 // get index of current hour
-var currentTimeIndex = allTimes.indexOf(currentHour);
+var currentTimeIndex = "12 PM";
 console.log("currentIndex: " + currentTimeIndex);
 // create array of work times
 var workTimes = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
